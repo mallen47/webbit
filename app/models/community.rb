0cfa8fb1c4ce7089c3867ec9cd5048ebc753ca74
Matt Allen <allen.matt@gmail.com>
2020-02-12 08:55:54 -0500

    WIP - Create communities
    Add scaffolding for communities and run migrations to add associations
    between communities to submissions and users to communities.

---
+++ app/models/community.rb
@@ -0,0 +1,5 @@
+class Community < ApplicationRecord
+	
+	belongs_to :user
+	has_many :submissions
+end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,6 +1,7 @@
 class Submission < ApplicationRecord
 	mount_uploader :submission_image, SubmissionImageUploader
 	belongs_to :user
+	belongs_to :community
 
 	validates :title, presence: true
 	validates :body, length: { maximum: 8000 }
