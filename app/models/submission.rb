ba41385af76b01639c5e1ae994e85425a20fce28
Matt Allen <allen.matt@gmail.com>
2020-02-21 09:42:40 -0500

    Create Comment Model
    Add comment model, update associations, run migration etc.

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,6 +2,7 @@ class Submission < ApplicationRecord
 	mount_uploader :submission_image, SubmissionImageUploader
 	belongs_to :user
 	belongs_to :community
+	has_many :comments, dependent: :destroy
 
 	validates :title, presence: true
 	validates :body, length: { maximum: 8000 }
